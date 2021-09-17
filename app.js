c1eca6faa39afc3d57d6b9774ebf01360e6476cf
Jade Yasmim <jadeyasmim20@gmail.com>
2021-09-16 23:46:20 -0300

    tratamento de login

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ var mongoose = require('mongoose');
 
 const app = express();
 const port = 8000;
+const passport = require('passport');
+const session = require('express-session');
+const flash = require('connect-flash'); // middleware
+//const cookie = require('cookie');
+//const setCookie = cookie.serialize('employee', 'true');
 
-mongoose.connect(
-  'mongodb+srv://jade_yasmim:jade_yasmim@cluster0.91nhm.mongodb.net/biblioteca?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+mongoose.connect('STRING DE CONEXAO', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const Livros = mongoose.model('Livros', {
   categoria: String,
@@ -23,18 +25,90 @@ const Livros = mongoose.model('Livros', {
 app.set('view engine', 'ejs');
 app.set('views', __dirname, '/views');
 app.use(express.urlencoded());
+app.use(flash());
+app.use(session({ secret: 'secret', resave: true, saveUninitialized: true })); // criando uma secret p/ session
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Página inicial');
+// Passport.json
+app.use(passport.initialize());
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: true
+  },
+  password: {
+    type: String,
+    require: true
+  }
+}); // queremos recuperar ali, o objeto service ligado pela primary key criada automaticamente
+
+UserSchema.methods.validPassword = function (pwd) {
+  // EXAMPLE CODE!
+  return this.password === pwd;
+};
+
+const User = mongoose.model('User', UserSchema);
+
+LocalStrategy = require('passport-local').Strategy;
+
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  })
+); // estratégias de autenticação
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
 });
 
-app.get('/livros', (req, res) => {
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+  req.logout();
+  res.send(
+    '<h1>Seja bem-vindo!<h1/><a href="/login">Login</a> <a href="/livros">Livros</a>'
+  );
+});
+
+app.get('/login', (req, res) => {
+  req.logout();
+
+  res.send(
+    '<form action="/login" method="post"><div><label>Username:</label><input type="text" name="username"/></div><div><label>Password:</label><input type="password" name="password"/></div><div><input type="submit" value="Log In"/></div></form>'
+  );
+});
+
+app.get('/livros', (req, res, err) => {
   let consulta = Livros.find({}, (err, livro) => {
     if (err) return res.status(500).send('Erro ao consultar Livro');
-
-    res.render('livros', { livro_itens: livro });
+    // SE USUARIO AUTENTICADO
+    if (req.isAuthenticated()) {
+      /// USUARIO LOGADO
+      //res.cookie('employee', 'true');
+      res.render('livros', { livro_itens: livro });
+    } else {
+      /// USUARIO NAO LOGADO
+      res.render('livrosUsuario', { livro_itens: livro });
+    }
   });
 });
 
@@ -88,6 +162,15 @@ app.post('/editarLivro', (req, res) => {
     });
   });
 });
+
+app.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/livros',
+    failureRedirect: '/login',
+    failureFlash: true
+  })
+);
 
 app.listen(port, () => {
   console.log('Servidor rodando na porta ' + port);
--- public/js/validacao.js
+++ public/js/validacao.js
@@ -8,14 +8,41 @@ $(document).ready(function () {
     );
   }
   $('#myInput').on('keyup', function () {
-    var value = $(this).val().toLowerCase();
+    // id  quando eu solto a teclado
+    var value = $(this).val().toLowerCase(); // procura por letras minusculas
     $('#myTable tr').filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+      // vai filtrar o que tiver na minha tr
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1); //toggle vai mostrar, o texto que eu digitar no campo bucar em letras minusculas, e o -1 é pra que ele procure a partir de 1 letra.
+    });
+  });
+
+  $('#mostrarLista').click(function () {
+    $('.modal').css({ display: 'initial' });
+  });
+
+  $('#logar').click(function () {
+    var usuario = document.getElementById('#login_nome').value;
+    var senha = document.getElementById('#senha').value;
+    alert('teste');
+    /**
+    if (usuario == 'adm' && senha == '123456') {
+      $('.esconder').show();
+      $('.modal').css({ display: 'none' });
+      alert('Administrador logado!');
+    } else {
+      $('.modal').css({ display: 'none' });
+      alert('Usuário logado!');
+    }
+
+ */
+  });
+
+  $(function () {
+    $('.close').click(function () {
+      $('.modal').css({ display: 'none' });
     });
   });
 });
-
-// validação categoria
 
 //função que valida campo nome
 function validaNome() {
@@ -28,6 +55,21 @@ function validaNome() {
     document.getElementById('nome').style.border = 'green solid';
   }
 }
+
+/**
+function getCookie() {
+  var cookies = document.cookie
+    .split(';')
+    .map(cookie => cookie.split('='))
+    .reduce(
+      (accumulator, [key, value]) => ({
+        ...accumulator,
+        [key.trim()]: decodeURIComponent(value)
+      }),
+      {}
+    );
+}
+ */
 
 // $('#nome').blur(function () {
 //   var nome = $('#nome').val();
